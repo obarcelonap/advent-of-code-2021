04eac99de0b390286c8f4e337e391a8df00ae3bf
Oriol Barcelona <obarcelonap@gmail.com>
2021-12-01 10:49:57 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(depths: List<Int>): Int = depths
+        .windowed(2)
+        .count { it[1] > it[0] }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(depths: List<Int>): Int = depths
+        .windowed(3) { it.sum() }
+        .windowed(2)
+        .count { it[1] > it[0] }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
