9d9748b0241c40d1d6a68c727ac8fd29b20ccf27
Oriol Barcelona <obarcelonap@gmail.com>
2021-12-14 12:57:40 +0100

    Day14 part2

--- src/Day14.kt
+++ src/Day14.kt
@@ -10,40 +10,45 @@ fun main() {
     }
 
 
-    fun steps(n: Int, template: String, insertionRules: Map<String, String>): Int {
-        tailrec fun step(n: Int, template: String): String {
+    fun steps(n: Int, template: String, insertionRules: Map<String, String>): Long {
+        tailrec fun step(n: Int, counts: Map<String, Long>): Map<String, Long> {
             if (n == 0) {
-                return template
+                return counts
             }
 
-            println("step $n template $template")
-
-            val nextTemplate = template
-                .windowed(2, partialWindows = true)
-                .joinToString("") {
-                    it[0] + (insertionRules[it] ?: "")
+            val newCounts =
+                counts.entries.fold<Map.Entry<String, Long>, MutableMap<String, Long>>(mutableMapOf()) { acc, (pair, count) ->
+                    val insertion = insertionRules[pair] ?: ""
+                    acc.merge(pair[0] + insertion, count, java.lang.Long::sum)
+                    acc.merge(insertion + pair[1], count, java.lang.Long::sum)
+                    acc
                 }
-            return step(n - 1, nextTemplate)
+            return step(n - 1, newCounts)
         }
 
-        val finalTemplate = step(n, template)
-        val elementOccurrences = finalTemplate.groupBy { it }.mapValues { it.value.size }
-        return elementOccurrences.maxOf { it.value } - elementOccurrences.minOf { it.value }
+        val initialPairCounts = template.windowed(2).groupBy { it }.mapValues { it.value.size.toLong() }
+        val pairCounts = step(n, initialPairCounts)
+
+        val charCounts = pairCounts.entries.fold(mutableMapOf<Char, Long>(template[0] to 1)) { acc, (pair, count) ->
+            acc.merge(pair[1], count, java.lang.Long::sum)
+            acc
+        }
+        return charCounts.maxOf { it.value } - charCounts.minOf { it.value }
     }
 
-    fun part1(template: String, insertionRules: Map<String, String>): Int {
+    fun part1(template: String, insertionRules: Map<String, String>): Long {
         return steps(10, template, insertionRules)
     }
 
-    fun part2(template: String, insertionRules: Map<String, String>): Int {
+    fun part2(template: String, insertionRules: Map<String, String>): Long {
         return steps(40, template, insertionRules)
     }
 
     // test if implementation meets criteria from the description, like:
     val (testTemplate, testInsertionRules) = parse(readInput("Day14_test"))
-    check(part1(testTemplate, testInsertionRules) == 1588)
+    check(part1(testTemplate, testInsertionRules) == 1588L)
 
     val (template, insertionRules) = parse(readInput("Day14"))
     println(part1(template, insertionRules))
-//    println(part2(template, insertionRules))
+    println(part2(template, insertionRules))
 }
